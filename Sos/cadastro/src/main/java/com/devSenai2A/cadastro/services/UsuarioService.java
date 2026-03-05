2cd333922f31797406d0ff0ad3835c310e1cbd0b
anna-xlv <Annal.guido1a@gmail.com>
2026-03-05 09:11:03 -0300

    quase finish

---
+++ Sos/cadastro/src/main/java/com/devSenai2A/cadastro/services/UsuarioService.java
@@ -0,0 +1,78 @@
+package com.devSenai2A.cadastro.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.devSenai2A.cadastro.entities.Usuario;
+import com.devSenai2A.cadastro.repositories.UsuarioRepository;
+
+@Service
+
+public class UsuarioService {
+   
+    @Autowired
+    private UsuarioRepository repository;
+    
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder; 
+   
+    public List<Usuario> listarTodos(){
+        return repository.findAll();
+    }
+   
+    public Usuario cadastrar(Usuario usuario) {
+        usuario.setSenha(new BCryptPasswordEncoder().encode(usuario.getSenha()));
+        return repository.save(usuario);
+    }
+    
+    
+    public Usuario atualizar(Long id, Usuario dados) {
+        Usuario usuario = repository.findById(id).orElse(null);
+        if (usuario == null) {
+            return null;
+        }
+
+
+    	usuario.setNome(dados.getNome());
+    	usuario.setEmail(dados.getEmail());
+    	usuario.setSenha(dados.getSenha());
+    	usuario.setPerfil(dados.getPerfil());
+    	usuario.setEndereco(dados.getEndereco());
+    	usuario.setBairro(dados.getBairro());
+    	usuario.setComplemento(dados.getComplemento());
+    	usuario.setCep(dados.getCep());
+    	usuario.setCidade(dados.getCidade());
+    	usuario.setEstado(dados.getEstado());
+
+    	return repository.save(usuario);
+    	}
+
+    	public boolean deletar(Long id) {
+    	if (!repository.existsById(id)) {
+    	return false;
+    	}
+
+    	repository.deleteById(id);
+    	return true;
+    	}
+    	
+    	  public Usuario buscarPorId(Long id) {
+    	        return repository.findById(id).orElse(null);
+    	    }
+    	
+    	public Usuario login(String email, String senha) {
+    		   		
+    		Usuario usuario =  repository.findByEmail(email);
+    		if (usuario == null) {
+    			return null;
+    		}
+            if (!passwordEncoder.matches(senha, usuario.getSenha())) {
+                return null;
+            }
+            return usuario;
+    	}
+    	    	
+}
